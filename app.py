3edc4f04386827ccc4e25e9ebca06f603772bbde
Tobias Böhnemann <tobiasb@Tobiass-MBP.fritz.box>
2019-08-13 12:20:28 +0200

    Added JSON unpacking

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
     if request.method == "GET":
         return render_template("main_page.html")
 
-    return get_deck_from_url(request.form["contents"])
+    create_printout(request.form["contents"])
     try:
         return send_file('LICENSE')
     except Exception as e:
@@ -20,9 +20,16 @@ def index():
 
 def create_printout(url):
     deck = get_deck_from_url(url)
+    generate_file(deck)
 
 def get_deck_from_url(url):
     deck_id = url.split("/")[-1]
     deck_url = 'https://www.keyforgegame.com/api/decks/' + deck_id.strip() + '/?links=cards'
     deck_page = requests.get(deck_url)
-    return str(deck_page.json())
+    return json.loads(deck_page)
+
+def generate_file(deck):
+    name = deck.get("data").get("name")
+    houses = deck.get("data").get("_links").get("houses")
+    card_index = deck.get("data").get("_links").get("cards")
+    card_data = deck.get("_linked").get("cards")
